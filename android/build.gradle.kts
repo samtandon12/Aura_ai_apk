70274f2c6f98310bb7d392a3ee7402fa6fab4d26
samtandon12 <samtandon71@gmail.com>
2026-09-18 21:29:52 +0530

    Force compileSdkVersion 36 on subprojects in root build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,6 +17,12 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+    if (project.hasProperty("android")) {
+        val androidExt = project.extensions.findByName("android")
+        if (androidExt is com.android.build.gradle.BaseExtension) {
+            androidExt.compileSdkVersion(36)
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
